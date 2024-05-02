62c7b2b26244665ed664af8e29a7900f13145371
kavi <kavikaran19v@gmail.com>
2024-05-02 12:11:13 +0530

    add background image

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,14 @@
 
 
+import { Box } from "@mui/material";
+import Image from "next/image";
+
+
+
 export default function Home() {
   return (
-    <div>
-      Login
-    </div>
+    <Box  >
+      <Image src="/hero.webp" alt="background image" fill={true}  />
+    </Box>
   );
 }
